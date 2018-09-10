5143b7147bf9e0698ae3993ba11c04f2ffd42186
homa <homa@redhat.com>
2018-09-10 11:59:51 -0400

    added redux and redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
